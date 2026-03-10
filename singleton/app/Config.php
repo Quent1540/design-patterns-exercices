a3d14b27516329c4d733aa54839cfebca76b4fd5
Quent1540 <dieudonnequent1@gmail.com>
2026-03-10 16:13:34 +0100

    singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -1,5 +1,24 @@
 <?php
-
-# TODO: Créer une classe Config en Singleton
-
 namespace App;
+
+final class Config{
+    private static $instance;
+    private $settings = [];
+
+    private function __construct()
+    {
+        $this->settings = require('../config/config.php');
+    }
+
+    public static function getInstance(): Config
+    {
+        if(!self::$instance){
+            self::$instance=new self();
+        }
+        return self::$instance;
+    }
+
+    public static function get(){
+        return self::$instance->settings["apiKey"];
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,17 @@
 <?php
 require('../vendor/autoload.php');
-
+use App\Config;
 
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+$config = Config::getInstance();
+echo("config key " . $config->get(). "<br><br>");
+
+$config2 = Config::getInstance();
+if($config===$config2)
+    echo("memes instances de config");
+else{
+    echo("instances differentes");
+}
