a5835a28445ab33f79c494a16d551de574948f1e
Quent1540 <dieudonnequent1@gmail.com>
2026-03-10 17:17:13 +0100

    builder

---
+++ builder/app/MySQLQueryBuilder.php
@@ -0,0 +1,33 @@
+<?php
+namespace App;
+
+class MySQLQueryBuilder{
+    private string $table;
+    private array $fields = ['*'];
+    private array $conditions = [];
+
+    public function table(string $table): self{
+        $this->table = $table;
+        return $this;
+    }
+
+    public function select(array $fields): self{
+        $this->fields = $fields;
+        return $this;
+    }
+
+    public function where(string $field, string $operator, $value): self{
+        $this->conditions[] = "$field $operator '$value'";
+        return $this;
+    }
+
+    public function build(): string {
+        $query = "SELECT " . implode(', ', $this->fields);
+        $query .= " FROM " . $this->table;
+
+        if(!empty($this->conditions)){
+            $query .= " WHERE " . implode(' AND ', $this->conditions);
+        }
+        return $query;
+    }
+}
--- builder/public/index.php
+++ builder/public/index.php
@@ -1,7 +1,21 @@
 <?php
+
+use App\MySQLQueryBuilder;
+
 require('../vendor/autoload.php');
 
 
 # TODO: Creer un QueryBuilder
 # Ecrire une requête en chainant des methodes
 # Afficher la requête
+$sql = (new MySQLQueryBuilder())
+    ->table('test')
+    ->select(['testselect', 'testselect2'])
+    ->build();
+echo('doit afficher select testselect,testselect2 from test: <br>' . $sql . '<br><br>');
+
+$sql2 = (new MySQLQueryBuilder())
+    ->table('test')
+    ->where('testfield', '=', 'testfieldegal')
+    ->build();
+echo('doit afficher select * from test where testfield = testfieldegal: <br>' . $sql2 . '<br>');
